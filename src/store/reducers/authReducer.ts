7933da2aa9ac6b0465041a39308a1c9d7100cf04
trikitirem <przetacznikp@gmail.com>
2021-12-22 00:49:14 +0100

    store setup

---
+++ src/store/reducers/authReducer.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface AuthState {
+    name?: string;
+    loading: boolean;
+}
+
+const initialState: AuthState = {
+    loading: false
+}
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        load: state => { loading: true }
+    }
+});
+
+export const { load } = authSlice.actions;
+
+export default authSlice.reducer;
