31fd167c81d4ce66225039107c800ba84139ab39
trikitirem <przetacznikp@gmail.com>
2021-12-31 12:06:21 +0100

    matching two users

--- src/store/actions/musiciansActions.ts
+++ src/store/actions/musiciansActions.ts
@@ -31,7 +31,7 @@ export const thunkSwipe = (like: boolean):
         dispatch: ThunkDispatch<RootState, unknown, AnyAction>,
         getState: () => RootState,
     ) => {
-        const { musicians } = getState();
+        const { musicians, user } = getState();
         if (musicians.data && musicians.data!.length !== 0) {
             try {
                 if (!like)
@@ -41,16 +41,38 @@ export const thunkSwipe = (like: boolean):
                     const swipe = await supabase.from("user").select("likes, matches").filter("user_id", "eq", musicians.data[0].user_id);
                     if (swipe.error) throw swipe.error;
 
-                    console.log(swipe.data);
+                    const you = await supabase.from("user").select("likes, matches").filter("user_id", "eq", supabase.auth.user()!.id);
+                    if (you.error) throw you.error;
 
 
                     if (swipe.data[0].likes.includes(supabase.auth.user()!.id)) {
                         //if you are liked: match 
 
-                    } else {
-                        //else add swipe to your likes
-                    }
+                        //update swiped person
 
+                        const swipeUpdate = await supabase.from("user").update({
+                            matches: [...swipe.data[0].matches, supabase.auth.user()!.id],
+                            likes: [...swipe.data[0].likes.filter((el: string) => el !== supabase.auth.user()!.id)]
+                        }).match({ user_id: musicians.data[0].user_id });
+                        if (swipeUpdate.error) throw swipeUpdate.error;
+
+
+
+                        //update your data
+                        const yoursUpdate = await supabase.from("user").update({
+                            matches: [...you.data[0].matches, musicians.data[0].user_id],
+                            likes: [...you.data[0].likes.filter((el: string) => el !== musicians.data![0].user_id)]
+                        }).match({ user_id: supabase.auth.user()!.id });
+                        if (yoursUpdate.error) throw yoursUpdate.error;
+
+
+                    } else {
+                        const yoursUpdate = await supabase.from("user").update({
+                            likes: [...you.data[0].likes, musicians.data![0].user_id]
+                        }).match({ user_id: supabase.auth.user()!.id });
+                        if (yoursUpdate.error) throw yoursUpdate.error;
+                    }
+                    dispatch({ type: loaded, payload: musicians.data.slice(1) });
                 }
             } catch (err: any) {
                 dispatch({ type: error, payload: err.message, });
